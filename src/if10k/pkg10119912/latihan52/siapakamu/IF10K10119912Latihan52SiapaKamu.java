cac110d467f7cd0d91839ede886b772e93703887
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-11-21 18:54:22 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan52/siapakamu/IF10K10119912Latihan52SiapaKamu.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan52.siapakamu;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */
+public class IF10K10119912Latihan52SiapaKamu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Dosen dosen = new Dosen();
+        dosen.setNIP("41227829930");
+        dosen.setNama("Rizki Adam Kurniawan");
+        dosen.setUmur(27);
+        dosen.setMataKuliah("PBO");
+        System.out.println("NIP DOSEN = " + dosen.getNIP());
+        dosen.siapaKamu();
+        dosen.mengajarApa();
+        
+        System.out.println("");
+        
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.setNIM("10116228");
+        mhs.setNama("Gandy Christian Situmorang");
+        mhs.setUmur(23);
+        mhs.setKelas("PBO6K");
+        System.out.println("NIM MAHASISWA = " + dosen.getNIP());
+        mhs.siapaKamu();
+        mhs.kelasApa();
+    }
+    
+}
---
+++ src/if10k/pkg10119912/latihan52/siapakamu/Manusia.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan52.siapakamu;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ */
+public class Manusia {
+    protected String nama;
+    protected int umur;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+    
+    public void siapaKamu(){
+        
+    }
+}
